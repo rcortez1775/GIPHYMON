719ef6bcdb53303df03756cb6fa00a2e11838dff
Robert Cortez <Nuhuhfoo@gmail.com>
2018-06-20 12:36:48 -0700

    final update

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
         $(this).addClass("active");
 
         var type = $(this).attr("data-type");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=KJ3A0XyIQUTOIQjcYv4jYAJroCV3nL0e";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=KJ3A0XyIQUTOIQjcYv4jYAJroCV3nL0e";
 
         $.ajax({
                 url: queryURL,
